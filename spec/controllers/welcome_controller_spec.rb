20056a6e7f2412c85da7138e9481d6236abbe482
teraJumper98193 <tomasz.swider@gmail.com>
2016-04-13 22:20:39 -0400

    Assignment 26 completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -20,6 +20,13 @@ end
        get :about
        expect(response).to render_template("about")
      end
+   end
+
+   describe "GET faq" do
+     it "renders the faq template" do
+       get :faq
+       expect(response).to render_template("faq")
+     end
    end 
 
 end
