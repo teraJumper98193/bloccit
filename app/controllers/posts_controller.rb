6fdf25e131ae8f0a094fbcc0005eef134d1603ba
teraJumper98193 <tomasz.swider@gmail.com>
2016-05-04 20:41:31 -0400

    Checkpoint 34 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,63 @@
 class PostsController < ApplicationController
-  def index
-     @posts = Post.all
-  end
-
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
   def create
-# #9
-  @post = Post.new
-  @post.title = params[:post][:title]
-  @post.body = params[:post][:body]
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+# #35
+    @post.topic = @topic
 
-# #10
-  if @post.save
-# #11
-    flash[:notice] = "Post was saved successfully."
-    redirect_to @post
-  else
-# #12
-    flash.now[:alert] = "There was an error saving the post. Please try again."
-    render :new
+    if @post.save
+      flash[:notice] = "Post was saved successfully."
+# #36
+
+      redirect_to [@topic, @post]
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
-end
 
   def edit
     @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated successfully."
+# #37
+
+      redirect_to [@post.topic, @post]
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+# #38
+    
+       redirect_to @post.topic
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
+  end
 end
-
-def update
-   @post = Post.find(params[:id])
-   @post.title = params[:post][:title]
-   @post.body = params[:post][:body]
-
-   if @post.save
-     flash[:notice] = "Post was updated successfully."
-     redirect_to @post
-   else
-     flash.now[:alert] = "There was an error saving the post. Please try again."
-     render :edit
-   end
- end
-
-
-def destroy
-   @post = Post.find(params[:id])
-
-# #8
-   if @post.destroy
-     flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-     redirect_to posts_path
-   else
-     flash.now[:alert] = "There was an error deleting the post."
-     render :show
-   end
- end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,97 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-# #8
-  let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+  # #12
+    let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
+  # #13
+    let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-    it "assigns [my_post] to @posts" do
-      get :index
-  # #9
-     expect(assigns(:posts)).to eq([my_post])
-    end
-  end
 
 
   # #1
   describe "GET new" do
      it "returns http success" do
-       get :new
+# #18
+       get :new, topic_id: my_topic.id
        expect(response).to have_http_status(:success)
      end
 
      # #2
           it "renders the #new view" do
-            get :new
+    # #19
+            get :new, topic_id: my_topic.id
             expect(response).to render_template :new
           end
 
      # #3
           it "instantiates @post" do
-            get :new
+    # #20
+            get :new, topic_id: my_topic.id
             expect(assigns(:post)).not_to be_nil
           end
         end
 
         describe "POST create" do
-     # #4
+
           it "increases the number of Post by 1" do
-            expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+  # #21
+            expect{post :create, topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
           end
 
-     # #5
+
           it "assigns the new post to @post" do
-            post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+  # #22
+            post :create, topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
             expect(assigns(:post)).to eq Post.last
           end
 
-     # #6
+
           it "redirects to the new post" do
-            post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-            expect(response).to redirect_to Post.last
+  # #23
+            post :create, topic_id: my_topic.id, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+  # #24
+            expect(response).to redirect_to [my_topic, Post.last]
+
           end
         end
 
     describe "GET show" do
     it "returns http success" do
-# #16
-    get :show, {id: my_post.id}
+# #15
+    get :show, topic_id: my_topic.id, id: my_post.id
     expect(response).to have_http_status(:success)
   end
   it "renders the #show view" do
-# #17
-    get :show, {id: my_post.id}
+# #16
+    get :show, topic_id: my_topic.id, id: my_post.id
     expect(response).to render_template :show
   end
 
     it "assigns my_post to @post" do
-    get :show, {id: my_post.id}
-# #18
+# #17
+    get :show, topic_id: my_topic.id, id: my_post.id
     expect(assigns(:post)).to eq(my_post)
   end
 end
 
 describe "GET edit" do
   it "returns http success" do
-    get :edit, {id: my_post.id}
+# #25
+    get :edit, topic_id: my_topic.id, id: my_post.id
     expect(response).to have_http_status(:success)
   end
 
   it "renders the #edit view" do
-    get :edit, {id: my_post.id}
-# #1
+# #26
+    get :edit, topic_id: my_topic.id, id: my_post.id
     expect(response).to render_template :edit
   end
 
-# #2
-  it "assigns post to be updated to @post" do
-    get :edit, {id: my_post.id}
 
+  it "assigns post to be updated to @post" do
+# #27
+    get :edit, topic_id: my_topic.id, id: my_post.id
     post_instance = assigns(:post)
 
     expect(post_instance.id).to eq my_post.id
@@ -104,10 +104,10 @@ describe "PUT update" do
     it "updates post with expected attributes" do
       new_title = RandomData.random_sentence
       new_body = RandomData.random_paragraph
+# #28
+      put :update, topic_id: my_topic.id, id: my_post.id, post: {title: new_title, body: new_body}
 
-      put :update, id: my_post.id, post: {title: new_title, body: new_body}
 
-# #3
       updated_post = assigns(:post)
       expect(updated_post.id).to eq my_post.id
       expect(updated_post.title).to eq new_title
@@ -118,25 +118,26 @@ describe "PUT update" do
       new_title = RandomData.random_sentence
       new_body = RandomData.random_paragraph
 
-# #4
-      put :update, id: my_post.id, post: {title: new_title, body: new_body}
-      expect(response).to redirect_to my_post
+# #29
+      put :update, topic_id: my_topic.id, id: my_post.id, post: {title: new_title, body: new_body}
+# #30
+      expect(response).to redirect_to [my_topic, my_post]
     end
   end
 
 describe "DELETE destroy" do
   it "deletes the post" do
-    delete :destroy, {id: my_post.id}
- # #6
+# #31
+    delete :destroy, topic_id: my_topic.id, id: my_post.id
     count = Post.where({id: my_post.id}).size
     expect(count).to eq 0
   end
 
-  it "redirects to posts index" do
-    delete :destroy, {id: my_post.id}
- # #7
-    expect(response).to redirect_to posts_path
+  it "redirects to topic show" do
+# #32
+  delete :destroy, topic_id: my_topic.id, id: my_post.id
+# #33
+  expect(response).to redirect_to my_topic
+    end
   end
-end
-
 end
