347fe8df6864f2016a437a4070764761adb18cb2
teraJumper98193 <tomasz.swider@gmail.com>
2016-05-07 19:16:32 -0400

    Assignment 35 completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,7 +7,15 @@ RSpec.describe Topic, type: :model do
   let(:public) { true }
   let(:topic) { Topic.create!(name: name, description: description) }
 
-  it { is_expected.to have_many(:posts) }
+  it { is_expected.to have_many(:topic) }
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(15) }
+
 
 # #1
   describe "attributes" do
