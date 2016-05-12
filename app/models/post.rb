020eaec6ccaea92901056e87e728a127501c98ad
teraJumper98193 <tomasz.swider@gmail.com>
2016-05-12 02:03:37 -0400

    Assignment 39 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+
+  default_scope { order('ordered_by_title') }
+  default_scope { order('ordered_by_reverse_created_at') }
+
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
