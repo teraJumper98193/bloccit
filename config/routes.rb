681eb161f55768f173facb39713c6c3a84f3044e
teraJumper98193 <tomasz.swider@gmail.com>
2016-05-31 20:46:04 -0400

    Assignment 49 completed

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
       resources :topics, except: [:edit, :new]
+      resources :posts, except: [:edit, :new]
     end
   end
 end
